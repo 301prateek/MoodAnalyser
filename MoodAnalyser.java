b2e6f43a300e8bdb9c0a27a31779a9f8445ddbea
301prateek <prateek.patil301@gmail.com>
2020-11-23 23:50:50 +0530

    Added: Custom Exception

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -47,4 +47,26 @@ public class MoodAnalyser {
 			return "Happy";
 		}
 	}
+
+
+	//Exception Handeling Using Custom Exception
+	public String analyseMood() throws MoodAnalysisException {
+		try {
+
+			if(message.length() == 0){
+				throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY, "Please Enter Proper Message");
+			}
+
+			if(message.contains("I Am In Sad Mood")) {
+
+					return "Sad";
+				}
+			else {
+				return "Happy";
+			}
+		}
+		catch(NullPointerException e) {
+			throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL, "Please Enter Proper Message");
+		}
+	}
 }

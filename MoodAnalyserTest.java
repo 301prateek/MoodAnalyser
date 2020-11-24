b8c1a1b4265160f80e8136704a3f9b5b314b34db
301prateek <prateek.patil301@gmail.com>
2020-11-24 19:28:37 +0530

    Added: Test Case For Empty Mood End Exception Handeling

---
+++ MoodAnalyserTest.java
@@ -0,0 +1,35 @@
+package com.moodanalyser.test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import com.moodanalyser.main.MoodAnalyser;
+import com.moodanalyser.main.MoodAnalysisException;
+
+import org.junit.Assert;
+
+public class MoodAnalyserTest  {
+
+	@Test
+	public void givenMood_WhenNull_ShouldReturnMessage(){
+
+		try {
+			MoodAnalyser moodanalyser = new MoodAnalyser(null);
+			Assert.assertEquals("Happy", moodanalyser.analyseMood());
+		}
+		catch(MoodAnalysisException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test
+	public void givenMood_WhenEmpty_ShouldReturnHappy() {
+
+		try {
+			MoodAnalyser moodanalyser = new MoodAnalyser("");
+			Assert.assertEquals("Happy",moodanalyser.analyseMood());
+		} catch (MoodAnalysisException e) {
+			e.printStackTrace();
+		}
+	}
+}
